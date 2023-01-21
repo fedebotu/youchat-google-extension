2efe34c3f384ee3c4892a79e2f97e34ce6491a4c
Federico Berto <berto.federico2@gmail.com>
2023-01-21 19:22:33 +0900

    Include messaging metadata; add Link object

--- src/messaging.ts
+++ src/messaging.ts
@@ -1,5 +1,15 @@
+import { List } from "postcss/lib/list";
+
 export interface Answer {
   text: string;
+  links: [];
+  status: string;
   messageId: string;
   conversationId: string;
+}
+
+export interface Link {
+  index: number;
+  name: string;
+  url: string;
 }
