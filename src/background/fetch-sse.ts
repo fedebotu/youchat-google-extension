2fea105fb549d3af1de3f0b10d4ff95e4eae7e82
Federico Berto <berto.federico2@gmail.com>
2023-01-21 19:23:15 +0900

    Pass full message to parser

--- src/background/fetch-sse.ts
+++ src/background/fetch-sse.ts
@@ -3,7 +3,7 @@ import { streamAsyncIterable } from "./stream-async-iterable.js";
 
 export async function fetchSSE(
   resource: string,
-  options: RequestInit & { onMessage: (message: string) => void }
+  options: RequestInit & { onMessage: (message: any) => void }
 ) {
   const { onMessage, ...fetchOptions } = options;
   const resp = await fetch(resource, fetchOptions);
@@ -12,9 +12,7 @@ export async function fetchSSE(
     throw new Error(detail || `${resp.status} ${resp.statusText}`);
   }
   const parser = createParser((event) => {
-    if (event.type === "event") {
-      onMessage(event.data);
-    }
+    onMessage(event); // we pass the data to
   });
   for await (const chunk of streamAsyncIterable(resp.body!)) {
     const str = new TextDecoder().decode(chunk);
